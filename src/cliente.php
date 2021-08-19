ad33c091945f66a9538ac3b56a95d7ee5078fe8f
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2021-08-19 18:28:55 -0300

    CRUD para cliente finalizado

--- src/cliente.php
+++ src/cliente.php
@@ -2,47 +2,147 @@
 <html>
   <head>
     <title>Clientes</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+    integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
   <body>
-    <?php require_once 'controllers/cliente_controller.php'; ?>
+    <?php require_once (__DIR__.'/controllers/cliente_controller.php'); ?>
 
-    <?php ?>
+    <?php
+    if(isset($_SESSION['message'])): ?>
+      <div class="alert alert-<?=$_SESSION['msg_type']?>">    
+        <?php 
+          echo $_SESSION['message'];
+          unset($_SESSION['message']);
+        ?>
+      </div>
+    <?php endif ?>
+
+    <div class="container">
+    <?php 
+      //Conectar ao banco 
+      $str = file_get_contents(__DIR__.'/../env.json');
+      $json = json_decode($str, true);
+      $c = oci_connect($json['username'], $json['password'], "bdengcomp_low");
+      if (!$c) {
+          $m = oci_error();
+          trigger_error("Não pôde conectar com o banco de dados: ". $m["message"], E_USER_ERROR);
+      }
+
+      // Modificar o formato da DATA
+      $s = oci_parse($c, "ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY'");
+      if (!$s) {
+        $m = oci_error($c);
+        trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+      }
+
+      $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+      if (!$r) {
+        $m = oci_error($r);
+        trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+      }
+
+
+      // Fazer o select
+      $s = oci_parse($c, "SELECT * from CLIENTE");
+      if (!$s) {
+        $m = oci_error($c);
+        trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+      }
+    
+      $r = oci_execute($s); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+      if (!$r) {
+        $m = oci_error($r);
+        trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+      }
+    ?>
+
+    <!-- Mostrar os dados !-->
+    <div class="row justifiy-content-center">
+      <table class="table">
+
+        <!-- Cabeçalhos !-->
+        <thead>
+          <tr>
+            <?php 
+              $ncols = oci_num_fields($s);              
+              for ($i = 1; $i <= $ncols; ++$i) {
+                $colname = oci_field_name($s, $i);
+                echo "<th>".htmlspecialchars($colname,ENT_QUOTES|ENT_SUBSTITUTE)."</th>\n";                      
+              }
+            ?>
+            <th colspan="2">Ação</th>
+          </tr>
+        </thead>
+
+        <!-- Dados !-->
+        <?php
+          while (($row = oci_fetch_array($s, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+            echo "<tr>\n";
+            foreach ($row as $item) {
+              echo "<td>";
+              echo $item !== null ? htmlspecialchars($item, ENT_QUOTES|ENT_SUBSTITUTE) : "&nbsp;";
+              echo "</td>\n";
+            }?>
+            <td> 
+              <a href="cliente.php?editar=<?php echo $row['CPF'];?>" class="btn btn-info">Editar</a>
+              <a href="controllers/cliente_controller.php?deletar=<?php echo $row['CPF'];?>" class="btn btn-danger">Deletar</a>
+            </td>
+            <?php
+            echo "</tr>\n";
+          }
+        ?>
+      </table>
+    </div>
 
     <div  class="row justify-content-center">
       <form action="controllers/cliente_controller.php" method="POST">
       <div class="form-group">
         <label>CPF</label>
-        <input type="text" name="cpf" class="form-control" placeholder="Insira o CPF">
+        <input type="text" name="cpf" class="form-control"
+         value="<?php echo $cpf; ?>" placeholder="Insira o CPF">
       </div>
       <div class="form-group">
         <label>Nome</label>
-        <input type="text" name="nome" class="form-control" placeholder="Insira o Nome">
+        <input type="text" name="nome" class="form-control" 
+         value="<?php echo $nome; ?>"placeholder="Insira o Nome">
       </div>
       <div class="form-group">
         <label>Data de Nascimento</label>
-        <input type="date" name="data_nascimento" class="form-control" placeholder="Insira a data de nascimento">
+        <input type="date" name="data_nascimento" class="form-control" 
+         value="<?php echo $data_nascimento; ?>"placeholder="Insira a data de nascimento">
       </div>
       <div class="form-group">
         <label>Email</label>
-        <input type="text" name="email" class="form-control" placeholder="Insira o Email">
+        <input type="text" name="email" class="form-control" 
+         value="<?php echo $email; ?>"placeholder="Insira o Email">
       </div>
       <div class="form-group">
         <label>Telefone</label>
-        <input type="text" name="telefone" class="form-control" placeholder="Insira o Telefone">
+        <input type="text" name="telefone" class="form-control" 
+         value="<?php echo $telefone; ?>"placeholder="Insira o Telefone">
       </div>
       <div class="form-group">
         <label>Endereço</label>
-        <input type="text" name="endereco" class="form-control" placeholder="Insira o Endereço">
+        <input type="text" name="endereco" class="form-control" 
+         value="<?php echo $endereco; ?>"placeholder="Insira o Endereço">
       </div>
       <div class="form-group">
+        <?php if($update == true): ?>
+          <button type="submit" class="btn btn-info" name="atualizar">Atualizar</button>
+        <?php else: ?>
         <button type="submit" class="btn btn-primary" name="inserir">Inserir</button>
+        <?php endif; ?>
       </div>
-
-    </form>
+      <div class="form-group">
+        <a href="../index.php" class="btn btn-primary">Retornar</a>
+      </div>
+      </form>
+    </div>
     </div>
   </body>
 </html>
--- src/controllers/cliente_controller.php
+++ src/controllers/cliente_controller.php
@@ -1,5 +1,5 @@
 <?php
-$str = file_get_contents('../../env.json');
+$str = file_get_contents(__DIR__.'/../../env.json');
 
 $json = json_decode($str, true);
 
@@ -9,6 +9,18 @@ if (!$c) {
     $m = oci_error();
     trigger_error("Não pôde conectar com o banco de dados: ". $m["message"], E_USER_ERROR);
 }
+
+session_start();
+
+$update = false;
+$cpf = '';
+$nome = '';
+$data_nascimento = '';
+$email = '';
+$telefone = '';
+$endereco = '';
+
+
 if (isset($_POST['inserir'])){
   $cpf = $_POST['cpf'];
   $nome = $_POST['nome'];
@@ -17,7 +29,7 @@ if (isset($_POST['inserir'])){
   $telefone = $_POST['telefone'];
   $endereco = $_POST['endereco'];
 
-  // Modificar o formato da DATA para aceitar
+  // Modificar o formato da DATA
   $s = oci_parse($c, "ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY'");
   if (!$s) {
     $m = oci_error($c);
@@ -45,7 +57,121 @@ if (isset($_POST['inserir'])){
 
   oci_commit($c);
 
-  print "Cliente (".$cpf.", ".$nome.", ". $data_nascimento.", 
-        ".$email. ", ".$telefone. ", ".$endereco. ") inserido.";
+  $_SESSION['message'] = "Cliente inserido!";
+  $_SESSION['msg_type'] = "success";
+
+  header("location: ../cliente.php");
+  exit();
+}
+
+if (isset($_GET['deletar'])){
+  $cpf = $_GET['deletar'];
+
+  $s = oci_parse($c, "DELETE FROM CLIENTE WHERE CPF = $cpf");
+  if (!$s) {
+    $m = oci_error($c);
+    trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+  if (!$r) {
+    $m = oci_error($r);
+    trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  oci_commit($c);
+
+  $_SESSION['message'] = "Cliente deletado!";
+  $_SESSION['msg_type'] = "danger";
+
+  header("location: ../cliente.php");
+  exit();
+}
+
+if (isset($_GET['editar'])){
+
+  // Modificar o formato da DATA
+  $s = oci_parse($c, "ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD'");
+  if (!$s) {
+    $m = oci_error($c);
+    trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+  if (!$r) {
+    $m = oci_error($r);
+    trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  
+  $cpf = $_GET['editar'];
+
+  $s = oci_parse($c, "SELECT * FROM CLIENTE WHERE CPF = $cpf");
+  if (!$s) {
+    $m = oci_error($c);
+    trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $r = oci_execute($s); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+  if (!$r) {
+    $m = oci_error($r);
+    trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $row = oci_fetch_array($s, OCI_ASSOC+OCI_RETURN_NULLS);
+  if($row != false){
+    $update = true;
+    $cpf = $row['CPF'];
+    $nome = $row['NOME'];
+    $data_nascimento = $row['DATA_NASCIMENTO'];
+    $email = $row['EMAIL'];
+    $telefone = $row['TELEFONE'];
+    $endereco = $row['ENDERECO'];
+  }
 
 }
+
+if (isset($_POST['atualizar'])){
+  $cpf = $_POST['cpf'];
+  $nome = $_POST['nome'];
+  $data_nascimento = date("d-m-Y", strtotime($_POST['data_nascimento']));
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $endereco = $_POST['endereco'];
+
+  // Modificar o formato da DATA
+  $s = oci_parse($c, "ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY'");
+  if (!$s) {
+    $m = oci_error($c);
+    trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+  if (!$r) {
+    $m = oci_error($r);
+    trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  //Inserir os dados
+  $s = oci_parse($c, "UPDATE CLIENTE SET cpf='$cpf', nome='$nome', data_nascimento='$data_nascimento', email='$email', telefone='$telefone', endereco='$endereco' where cpf=$cpf");
+  if (!$s) {
+      $m = oci_error($c);
+      trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
+  if (!$r) {
+    $m = oci_error($r);
+    trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+  }
+
+  oci_commit($c);
+
+  $_SESSION['message'] = "Cliente atualizado!";
+  $_SESSION['msg_type'] = "warning";
+
+  header("location: ../cliente.php");
+  exit();
+}
+
+?>
