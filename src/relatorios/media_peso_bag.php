20c40f8dd201fe84e10960cf86868c8fe10080e9
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2021-08-29 23:44:33 -0300

    filtro peso cia aerea adicionado

--- src/relatorios/media_peso_bag.php
+++ src/relatorios/media_peso_bag.php
@@ -10,41 +10,89 @@
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
   <body>
+
+    <?php
+      session_start();
+
+      if(isset($_SESSION['message'])): ?>
+        <div class="alert alert-<?=$_SESSION['msg_type']?>">    
+          <?php 
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+          ?>
+        </div>
+    <?php endif ?>
+
     <div class="container">
     <?php 
+
+      $filtrar = isset($_POST['filtrar']);
+      $id = '';
+
       //Conectar ao banco 
       $str = file_get_contents(__DIR__.'/../../env.json');
       $json = json_decode($str, true);
       $c = oci_connect($json['username'], $json['password'], "bdengcomp_low");
       if (!$c) {
           $m = oci_error();
-          trigger_error("Não pôde conectar com o banco de dados: ". $m["message"], E_USER_ERROR);
+          $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+          $_SESSION['msg_type'] = "danger";
+          header("location: media_peso_bag.php");
+          exit();      
       }
 
       // Modificar o formato do Timestamp
       $s = oci_parse($c, "alter SESSION set NLS_TIMESTAMP_FORMAT = 'DD-MM-YYYY HH24:MI'");
       if (!$s) {
         $m = oci_error($c);
-        trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+        $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+        $_SESSION['msg_type'] = "danger";
+        header("location: media_peso_bag.php");
+        exit();    
       }
 
       $r = oci_execute($s, OCI_NO_AUTO_COMMIT); // for PHP <= 5.3.1 use OCI_DEFAULT instead
       if (!$r) {
         $m = oci_error($s);
-        exit();
+        $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+        $_SESSION['msg_type'] = "danger";
+        header("location: media_peso_bag.php");
+        exit();    
       }
 
-      // Fazer o select
-      $s = oci_parse($c, "SELECT * from peso_medio_bag");
-      if (!$s) {
-        $m = oci_error($c);
-        trigger_error("Não pôde compilar a sentença: ". $m["message"], E_USER_ERROR);
+      if (!$filtrar){
+        // Fazer o select
+        $s = oci_parse($c, "SELECT * from peso_medio_bag");
+        if (!$s) {
+          $m = oci_error($c);
+          $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+          $_SESSION['msg_type'] = "danger";
+          header("location: media_peso_bag.php");
+          exit();      
+        }
+      }else{
+        $id = $_POST["id"];
+        // Fazer o select
+        $s = oci_parse($c, "SELECT CIA_AEREA, AVG(PESO_BAG) MEDIA_PESO_BAG
+                            FROM aeroportos_destinos
+                            GROUP BY CIA_AEREA
+                            HAVING cia_aerea = $id");
+        if (!$s) {
+          $m = oci_error($c);
+          $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+          $_SESSION['msg_type'] = "danger";
+          header("location: media_peso_bag.php");
+          exit();      
+        }
       }
-    
+
       $r = oci_execute($s); // for PHP <= 5.3.1 use OCI_DEFAULT instead
       if (!$r) {
-        $m = oci_error($r);
-        trigger_error("Não pôde executar a sentença: ". $m["message"], E_USER_ERROR);
+        $m = oci_error($s);
+        $_SESSION['message'] = "Erro! Mensagem de erro: ".$m['message'];
+        $_SESSION['msg_type'] = "danger";
+        header("location: media_peso_bag.php");
+        exit();    
       }
     ?>
 
@@ -80,12 +128,25 @@
         ?>
       </table>
     </div>
-
-
-    <div class="form-group">
+    <div  class="row justify-content-center">
+      <form action="media_peso_bag.php" method="POST">
+      <div class="form-group">
+      <h2>Filtrar CIA Aérea</h2>
+      <label>ID CIA Aérea</label>
+          <input type="number" name="id" class="form-control"
+          value="<?php echo $id; ?>" placeholder="Insira o ID da CIA Aérea">
+      </div>
+      <div class="form-group">
+      <button type="submit" class="btn btn-primary" name="filtrar">Filtrar</button>
+      <?php if($filtrar): ?>
+          <button type="submit" class="btn btn-info" name="limpar_filtro">Limpar Filtro</button>
+        <?php endif; ?>
+      </div>
+      <div class="form-group">
         <a href="../../index.php" class="btn btn-primary">Retornar</a>
+      </div>
+      </form>
     </div>
-
     </div>
   </body>
 </html>
